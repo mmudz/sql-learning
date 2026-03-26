e93e942bddfeee77603550773e70e0a1cb21bcd7
mmudz <mudzmichal@gmail.com>
2026-03-26 19:44:25 +0100

    Update 002_SELECT_NAMES.sql

--- 01_SQL_ZOO/002_SELECT_NAMES.sql
+++ 01_SQL_ZOO/002_SELECT_NAMES.sql
@@ -36,3 +36,67 @@ SELECT name FROM world
   
 SELECT name FROM world
     WHERE name LIKE '_t%';
+
+-- 09. Find the countries that have two "o" characters separated by two others.
+
+SELECT name FROM world
+  WHERE name LIKE '%o__o%';
+
+-- 10. Find the countries that have exactly four characters.
+SELECT name FROM world
+    WHERE name LIKE '____';
+
+-- 11. Find the country where the name is the capital city.
+
+SELECT name FROM world
+    WHERE name = capital
+
+--12. Find the country where the capital is the country plus "City".
+
+SELECT name FROM world
+WHERE capital LIKE '% City'
+AND capital LIKE CONCAT(name, '%');
+
+--13. Find the capital and the name where the capital includes the name of the country.
+
+SELECT capital, name FROM world
+WHERE capital LIKE CONCAT(name, '%')
+
+--14. Find the capital and the name where the capital is an extension of name of the country.
+
+SELECT capital, name FROM world
+WHERE capital LIKE CONCAT('%', name, '%')
+AND name != capital
+
+--15. Show the name and the extension where the capital is a proper (non-empty) extension of name of the country.
+
+SELECT name, 
+REPLACE(capital, name, '')
+FROM world
+WHERE capital LIKE CONCAT('%', name, '%')
+AND name != capital
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
